0e37c2996b27bac0ce44f7774f2a89a4283a57e8
Jie Ma <ma333jie@berkeley.edu>
2020-05-06 19:45:42 -0700

    finish profile

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import TopNavBar from './TopNavBar';
+import Main from './Main';
+
+function App() {
+    return (
+        <div className="App">
+            <TopNavBar />
+            <Main />
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
