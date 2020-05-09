4b367aa548ae0230bb528b514417579b45fb4c47
Jie Ma <ma333jie@berkeley.edu>
2020-05-09 14:32:53 -0700

    finish button UI

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
+import nba from '../nba-client';
+import { PROFILE_PIC_URL_PREFIX } from '../constants';
 
 class SearchBar extends Component {
+
+
+
     render() {
         return (
-            <div>
-                search bar
-            </div>
+
+            <AutoComplete
+                className="search-bar"
+
+
+                placeholder="Search NBA Player"
+                size="large"
+                optionLabelProp="value"
+            >
+
+                <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+            </AutoComplete>
+
+
         );
     }
 }
